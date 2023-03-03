93937684642362ac46a980027ce153db3a37ab9c
mechneeraj <118884666+mechneeraj@users.noreply.github.com>
2023-03-03 11:58:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,19 @@ import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
-        super(props)
+        super(props) //js classes 
         this.state = {
             renderBall: false,
             posi : 0,
-            ballPosition: { left: "0px" }
+            ballPosition: { left: "0px" } ///increae this to move the ball towards right 
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
     };
 
     buttonClickHandler() {
-   
+        this.setState({renderBall: true});
+        /* setState({...state, renderBall: true}) functional code*/
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
@@ -25,8 +26,13 @@ class App extends Component {
     }
 
     // bind ArrowRight keydown event
+    //useEffecrt with a bblank array
     componentDidMount() {
-      
+      document.addEventListener("keydown", (e) => {
+        if(e.key === "ArrowRight"){
+            this.setState({ballPosition: {left: this.state.posi+5 + "px"}, posi: this.state.posi + 5})
+        }
+      })
     }
 
     render() {
@@ -37,6 +43,5 @@ class App extends Component {
         )
     }
 }
-
 
 export default App;
